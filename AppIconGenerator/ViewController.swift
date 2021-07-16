72a2364c25cea9076ebc416456470a3f513296b5
krishpranav <krisna.pranav@gmail.com>
2021-07-16 17:56:57 +0530

    ViewController: some IBOutlet functions for image generator in mac

--- AppIconGenerator/ViewController.swift
+++ AppIconGenerator/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: NSViewController {
     @IBOutlet weak var labelType: NSTextField!
     @IBOutlet weak var imageMain: NSImageView!
     
-    
+    // mac
+    @IBOutlet weak var image016A: NSImageView!
+    @IBOutlet weak var image016B: NSImageView!
+    @IBOutlet weak var image032A: NSImageView!
+    @IBOutlet weak var image032B: NSImageView!
+    @IBOutlet weak var image128A: NSImageView!
+    @IBOutlet weak var image128B: NSImageView!
+    @IBOutlet weak var image256A: NSImageView!
+    @IBOutlet weak var image256B: NSImageView!
+    @IBOutlet weak var image512A: NSImageView!
+    @IBOutlet weak var image512B: NSImageView!
 }
 
