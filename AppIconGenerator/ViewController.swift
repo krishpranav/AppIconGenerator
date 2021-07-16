98760d210c32a384356759f5fd30cb7de16d16ac
krishpranav <krisna.pranav@gmail.com>
2021-07-16 17:53:51 +0530

    ViewController: var values

--- AppIconGenerator/ViewController.swift
+++ AppIconGenerator/ViewController.swift
@@ -9,19 +9,10 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
+    
+    var action = 0
+    var image = NSImage()
+    
+    
 }
 
